fd90c2427fc27ec6c87fc980c9676d6f18dcb0ce
Jeremy Bass <jeremybass26@gmail.com>
2013-09-23 08:00:14 -0700

    ini

---
+++ app/code/local/Wsu/IRI/Model/IRI.php
@@ -0,0 +1,12 @@
+<?php
+
+class Wsu_IRI_Model_IRI extends Mage_Core_Model_Abstract
+{
+	public function _construct()
+    {
+    	
+        parent::_construct();
+        $this->_init('iri/iri');
+        
+    }
+}
---
+++ app/code/local/Wsu/IRI/Model/Method/IRI.php
@@ -0,0 +1,10 @@
+<?php
+
+class Wsu_IRI_Model_Method_IRI extends Mage_Payment_Model_Method_Abstract
+{
+	 protected $_code  = 'iri';
+    protected $_isInitializeNeeded      = true;
+     protected $_canUseInternal          = true;
+     protected $_canUseForMultishipping  = true;
+ 
+}
