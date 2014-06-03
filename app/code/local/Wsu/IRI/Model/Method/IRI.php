e5abed80aa0c5b0c54841ff97234c4dd6f8a21f3
jeremyBass <jeremybass@cableone.net>
2014-06-03 13:27:54 -0700

    tries to update the forms in general

--- app/code/local/Wsu/IRI/Model/Method/IRI.php
+++ app/code/local/Wsu/IRI/Model/Method/IRI.php
@@ -1,7 +1,37 @@
 <?php
 class Wsu_IRI_Model_Method_IRI extends Mage_Payment_Model_Method_Abstract {
     protected $_code = 'iri';
-    protected $_isInitializeNeeded = true;
+    /*protected $_isInitializeNeeded = true;
     protected $_canUseInternal = true;
     protected $_canUseForMultishipping = true;
+	
+    protected $_isGateway               = false;
+	protected $_canUseCheckout          = true;
+	protected $_formBlockType = 'iri/form_iri';*/
+    //protected $_infoBlockType = 'iri/info_iri';
+	
+	public function assignData($data){
+        if (!($data instanceof Varien_Object)) {
+            $data = new Varien_Object($data);
+        }
+        $info = $this->getInfoInstance();
+        $info->setCheckNo($data->getCheckNo())->setCheckDate($data->getCheckDate());
+        return $this;
+    }
+
+    public function validate(){
+        parent::validate();
+        $info = $this->getInfoInstance();
+
+        $no = $info->getCheckNo();
+        $date = $info->getCheckDate();
+        if(empty($no) || empty($date)){
+            $errorCode = 'invalid_data';
+            $errorMsg = $this->_getHelper()->__('Check No and Date are required fields');
+        }
+        if($errorMsg){
+            Mage::throwException($errorMsg);
+        }
+        return $this;
+    }
 }
